cd948cae8cbd23bba78f5cb79843cad273b1b830
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-08-01 14:09:03 -0500

    up to v2.4.0

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 BRANCH=stable
-VER=2.3.1
+VER=2.4.0
 MIRROR=https://github.com/linkerd/linkerd2/releases/download/stable-${VER}
 
 dl()
@@ -8,13 +8,13 @@ dl()
     OS=$1
     SUFFIX=${2:-}
     URL=$MIRROR/linkerd2-cli-$BRANCH-$VER-$OS$SUFFIX.sha256
-    printf "    # %s\n" $URL
-    printf "    %s: sha256:%s\n" $OS `curl -sSL $URL | awk '{print $1}'`
+    printf "      # %s\n" $URL
+    printf "      %s: sha256:%s\n" $OS `curl -sSL $URL | awk '{print $1}'`
 
 }
 
-printf "%s:\n" $BRANCH
-printf "  '%s':\n" $VER
+printf "  %s:\n" $BRANCH
+printf "    '%s':\n" $VER
 dl darwin
 dl linux
 dl windows .exe
