08defd3b72c3c4f409c7b4a6f53d050b090bf580
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-11-03 22:42:10 -0500

    up to v2.6.0. added edge checksums.

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,21 +1,35 @@
 #!/usr/bin/env sh
-BRANCH=stable
-VER=2.4.0
-MIRROR=https://github.com/linkerd/linkerd2/releases/download/stable-${VER}
+#set -x
+MIRROR=https://github.com/linkerd/linkerd2/releases/download
 
 dl()
 {
-    OS=$1
-    SUFFIX=${2:-}
-    URL=$MIRROR/linkerd2-cli-$BRANCH-$VER-$OS$SUFFIX.sha256
-    printf "      # %s\n" $URL
-    printf "      %s: sha256:%s\n" $OS `curl -sSL $URL | awk '{print $1}'`
+    local branch=$1
+    local ver=$2
+    local os=$3
+    local suffix=${4:-}
+    local url=$MIRROR/${branch}-${ver}/linkerd2-cli-$branch-$ver-$os$suffix.sha256
+    printf "      # %s\n" $url
+    printf "      %s: sha256:%s\n" $os `curl -sSL $url | awk '{print $1}'`
 
 }
 
-printf "  %s:\n" $BRANCH
-printf "    '%s':\n" $VER
-dl darwin
-dl linux
-dl windows .exe
+dl_ver() {
+    local branch=$1
+    local ver=$2
+    printf "    '%s':\n" $ver
+    dl $branch $ver darwin
+    dl $branch $ver linux
+    dl $branch $ver windows .exe
+}
 
+printf "  %s:\n" stable
+#dl_ver stable 2.3.1
+#dl_ver stable 2.3.2
+#dl_ver stable 2.4.0
+#dl_ver stable 2.5.0
+dl_ver stable ${1:-2.6.0}
+#printf "  %s:\n" edge
+#dl_ver edge 19.10.3
+#dl_ver edge 19.10.4
+#dl_ver edge 19.10.5
