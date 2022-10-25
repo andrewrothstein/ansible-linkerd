98b60deea7146ae78c221d5f9d9f8ec7e1243b11
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-10-25 16:07:44 -0400

    linkerd:2.12.2 ansible_{sys,arch} rewrite checksums

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -e
 #set -x
 MIRROR=https://github.com/linkerd/linkerd2/releases/download
@@ -8,25 +8,41 @@ dl()
     local branch=$1
     local ver=$2
     local os=$3
-    local suffix=${4:-}
-    local url=$MIRROR/${branch}-${ver}/linkerd2-cli-$branch-$ver-$os$suffix.sha256
+    local arch=$4
+    local suffix=${5:-}
+    local platform="${os}-${arch}"
+    local infix=$platform
+    if [ "${arch}" = "amd64" ] && [ "$os" = "darwin" ] || [ "$os" = "windows" ];
+    then
+        infix="$os"
+    fi
+    local url=$MIRROR/${branch}-${ver}/linkerd2-cli-$branch-$ver-$infix$suffix.sha256
     printf "      # %s\n" $url
-    printf "      %s: sha256:%s\n" $os $(curl -sSLf $url | awk '{print $1}')
+    printf "      %s: sha256:%s\n" $platform $(curl -sSLf $url | awk '{print $1}')
 }
 
 dl_ver() {
     local branch=$1
     local ver=$2
     printf "    '%s':\n" $ver
-    dl $branch $ver darwin
-    dl $branch $ver linux-amd64
-    dl $branch $ver linux-arm
-    dl $branch $ver linux-arm64
-    dl $branch $ver windows .exe
+    dl $branch $ver darwin amd64
+    dl $branch $ver darwin arm64
+    dl $branch $ver linux amd64
+    dl $branch $ver linux arm
+    dl $branch $ver linux arm64
+    dl $branch $ver windows amd64 .exe
 }
 
 printf "  %s:\n" stable
+dl_ver stable 2.10.1
+dl_ver stable 2.10.2
+dl_ver stable 2.11.0
+dl_ver stable 2.11.1
+dl_ver stable 2.11.2
 dl_ver stable 2.11.3
-dl_ver stable ${1:-2.11.4}
+dl_ver stable 2.11.4
+dl_ver stable 2.11.5
+dl_ver stable 2.12.1
+dl_ver stable ${1:-2.12.2}
 #printf "  %s:\n" edge
 #dl_ver edge 20.1.4
